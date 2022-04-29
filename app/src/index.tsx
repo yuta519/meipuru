9c5bfc24aa8140d454ccc0ba37864e30ff2ce029
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-04-29 18:19:01 +0900

    add some template files

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+
+import { TopPage } from "./scripts/pages";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    test
+    <TopPage />
   </React.StrictMode>
 );
 
---
+++ app/src/scripts/pages/index.tsx
@@ -0,0 +1,5 @@
+// import React from "react";
+
+export const TopPage = () => {
+  return <div>This is a test site</div>;
+};
