825e59b9849c0be77498c4db53b92106ef8e3873
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-05-02 09:20:47 +0900

    add footer

---
+++ app/src/scripts/components/navigations/GlobalFooter.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import styled from "styled-components";
+
+export const GlobalFooter = () => {
+  return (
+    <>
+      <StyledFooter>
+        <Typography variant="h4" color="white" align="left">
+          meipuru
+        </Typography>
+        <StyledGrid container>
+          <Grid item xs={2}>
+            <Typography variant="body1" color="white" align="left">
+              What is meipuru
+            </Typography>
+          </Grid>
+          <Grid item xs={2}>
+            <Typography variant="body1" color="white" align="left">
+              About Us
+            </Typography>
+          </Grid>
+          <Grid item xs={2}>
+            <Typography variant="body1" color="white" align="left">
+              Contact
+            </Typography>
+          </Grid>
+        </StyledGrid>
+        <StyledCopyright>
+          <Typography variant="body2" color="white" align="center">
+            Copyright © meipuru {new Date().getFullYear()}
+          </Typography>
+        </StyledCopyright>
+      </StyledFooter>
+    </>
+  );
+};
+
+const StyledFooter = styled.section`
+  padding: 4em;
+  background: #565252;
+`;
+
+const StyledGrid = styled(Grid)`
+  margin-top: 2em;
+`;
+
+const StyledCopyright = styled.div`
+  margin-top: 2em;
+`;
--- app/src/scripts/pages/Top/index.tsx
+++ app/src/scripts/pages/Top/index.tsx
@@ -1,10 +1,12 @@
 import { GlobalHeader } from "../../components/navigations/GlobalHeader";
+import { GlobalFooter } from "../../components/navigations/GlobalFooter";
 
 export const TopPage = () => {
   return (
     <>
       <GlobalHeader />
       <div>This is a top page</div>
+      <GlobalFooter />
     </>
   );
 };
